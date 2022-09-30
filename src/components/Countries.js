d106e0049465a5a08065bd075537c28c74ccf79a
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-09-30 22:46:34 +0100

    modified:   src/components/Countries.js

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -12,42 +12,43 @@ const Countries = ({ countries, loading }) => {
   } else {
     return (
       <>
-        <Paper
-          sx={{ flexGrow: 1, mt: 6, backgroundColor: "LightGray" }}
-          elevation={0}
+        <Grid
+          container
+          rowSpacing={{ xs: 5 }}
+          columnSpacing={{ xs: 12 }}
+          columns={12}
         >
-          <Grid container rowSpacing={{ xs: 5 }} columns={12}>
-            {/* xs, extra-small: 0px
+          {/* xs, extra-small: 0px
               sm, small: 600px
               md, medium: 900px
               lg, large: 1200px
               xl, extra-large: 1536px */}
-            {countries.map((country, index) => (
-              <Grid
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                xs={12}
-                sm={6}
-                md={4}
-                lg={3}
-                key={index}
-              >
-                <Link to={`details/${country.name.common}`}>
-                  <Country
-                    key={index}
-                    country={country}
-                    name={country.name.common}
-                    population={country.population}
-                    region={country.region}
-                    capital={country.capital}
-                    flag={country.flags.svg}
-                  />
-                </Link>
-              </Grid>
-            ))}
-          </Grid>
-        </Paper>
+          {countries.map((country, index) => (
+            <Grid
+              display="flex"
+              justifyContent="center"
+              // justifyContent="space-between"
+              alignItems="center"
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              key={index}
+            >
+              <Link to={`details/${country.name.common}`}>
+                <Country
+                  key={index}
+                  country={country}
+                  name={country.name.common}
+                  population={country.population}
+                  region={country.region}
+                  capital={country.capital}
+                  flag={country.flags.svg}
+                />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
       </>
     );
   }
