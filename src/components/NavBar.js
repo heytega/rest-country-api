1900689580c1584fb93c7c65a406fe105fba5ee9
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-09-30 22:45:20 +0100

    modified:   src/components/NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,14 +12,12 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 
 const NavBar = ({ toggleDarkMode, darkMode }) => {
   return (
-    // <Paper sx={{ flexGrow: 1 }}>
     <AppBar position="static">
-      <Container maxWidth="xlg">
+      <Container maxWidth="xl">
         <Toolbar sx={{ mt: 0.5, mb: 0.5 }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Where in the world?
           </Typography>
-          {/* <IconButton></IconButton> */}
           <DarkModeOutlinedIcon
             onClick={toggleDarkMode}
             sx={{ color: darkMode ? "white" : "DarkBlueT", cursor: "pointer" }}
@@ -35,7 +33,6 @@ const NavBar = ({ toggleDarkMode, darkMode }) => {
         </Toolbar>
       </Container>
     </AppBar>
-    // {/* </Paper> */}
   );
 };
 
