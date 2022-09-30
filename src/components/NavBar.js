ac611f1a551665a2e26af1ff666dd2b845ac1c60
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-09-30 12:46:51 +0100

    modified:   src/components/NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import {
   AppBar,
   Toolbar,
   Typography,
+  Container,
   IconButton,
   Paper,
 } from "@mui/material";
@@ -13,24 +14,26 @@ const NavBar = ({ toggleDarkMode, darkMode }) => {
   return (
     // <Paper sx={{ flexGrow: 1 }}>
     <AppBar position="static">
-      <Toolbar sx={{ mt: 0.5, mb: 0.5, mr: 6.5, ml: 6.5 }}>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Where in the world?
-        </Typography>
-        {/* <IconButton></IconButton> */}
-        <DarkModeOutlinedIcon
-          onClick={toggleDarkMode}
-          sx={{ color: darkMode ? "white" : "DarkBlueT", cursor: "pointer" }}
-        />
-        <Typography
-          variant="h6"
-          component="div"
-          onClick={toggleDarkMode}
-          sx={{ color: darkMode ? "white" : "DarkBlueT", cursor: "pointer" }}
-        >
-          {darkMode ? "Dark Mode" : "Light Mode"}
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="xlg">
+        <Toolbar sx={{ mt: 0.5, mb: 0.5 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Where in the world?
+          </Typography>
+          {/* <IconButton></IconButton> */}
+          <DarkModeOutlinedIcon
+            onClick={toggleDarkMode}
+            sx={{ color: darkMode ? "white" : "DarkBlueT", cursor: "pointer" }}
+          />
+          <Typography
+            variant="h6"
+            component="div"
+            onClick={toggleDarkMode}
+            sx={{ color: darkMode ? "white" : "DarkBlueT", cursor: "pointer" }}
+          >
+            {darkMode ? "Dark Mode" : "Light Mode"}
+          </Typography>
+        </Toolbar>
+      </Container>
     </AppBar>
     // {/* </Paper> */}
   );
