5eda4fa00856a0a2c3da9a74781840153217b007
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-10-01 04:17:55 +0100

    modified:   src/components/NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,14 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 
 const NavBar = ({ toggleDarkMode, darkMode }) => {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
+    <AppBar position="static" sx={{ backgroundColor: "primary.main" }}>
+      <Container
+        maxWidth="xl"
+        // sx={{ display: "flex", flexDirection: "row", mt: 2, mb: 2 }}
+        // sx={{
+        //   backgroundColor: "primary.main",
+        // }}
+      >
         <Toolbar sx={{ mt: 0.5, mb: 0.5 }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Where in the world?
