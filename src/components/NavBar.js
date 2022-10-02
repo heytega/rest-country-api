f6c85fc68c5f1b5cacebcf01d3db94442976eb49
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-10-02 20:57:19 +0100

    modified:   src/components/NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,24 +7,48 @@ import {
   Container,
   IconButton,
   Paper,
+  Button,
 } from "@mui/material";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 
 const NavBar = ({ toggleDarkMode, darkMode }) => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: "primary.main" }}>
-      <Container
-        maxWidth="xl"
-        // sx={{ display: "flex", flexDirection: "row", mt: 2, mb: 2 }}
-        // sx={{
-        //   backgroundColor: "primary.main",
-        // }}
+    <AppBar position="static">
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+        }}
       >
-        <Toolbar sx={{ mt: 0.5, mb: 0.5 }}>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Where in the world?
-          </Typography>
-          <DarkModeOutlinedIcon
+        <Container
+          maxWidth="xl"
+          // sx={{ display: "flex", flexDirection: "row", mt: 2, mb: 2 }}
+          sx={{
+            backgroundColor: "primary.main",
+          }}
+        >
+          <Toolbar sx={{ mt: 0.5, mb: 0.5, backgroundColor: "primary.main" }}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Where in the world?
+            </Typography>
+
+            <Button
+              variant="outlined"
+              startIcon={<DarkModeOutlinedIcon />}
+              onClick={toggleDarkMode}
+              sx={{
+                // mt: 4,
+                // mb: 6,
+                // pr: 3.5,
+                // pl: 3.5,
+                // pt: 1.3,
+                // pb: 1.3,
+                color: darkMode ? "white" : "#0000",
+                borderColor: "#fff",
+              }}
+            >
+              {darkMode ? "Dark Mode" : "Light Mode"}
+            </Button>
+            {/* <DarkModeOutlinedIcon
             onClick={toggleDarkMode}
             sx={{ color: darkMode ? "white" : "DarkBlueT", cursor: "pointer" }}
           />
@@ -35,9 +59,10 @@ const NavBar = ({ toggleDarkMode, darkMode }) => {
             sx={{ color: darkMode ? "white" : "DarkBlueT", cursor: "pointer" }}
           >
             {darkMode ? "Dark Mode" : "Light Mode"}
-          </Typography>
-        </Toolbar>
-      </Container>
+          </Typography> */}
+          </Toolbar>
+        </Container>
+      </Box>
     </AppBar>
   );
 };
