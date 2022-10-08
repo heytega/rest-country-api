299d3505f1185964ed33a93c4778f2f283b0dd7e
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-10-08 23:04:51 +0100

    modified:   src/components/Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,11 +3,6 @@ import { Box, Typography, CircularProgress } from "@mui/material";
 
 const Loading = () => {
   return (
-    // <Box
-    //   sx={{ backgroundColor: "primary.light", pt: 50, pb: 50, margin: "auto" }}
-    // >
-    //   <Typography variant="h6">Loading...</Typography>
-    // </Box>
     <Box
       sx={{ display: "flex", zIndex: "3", backgroundColor: "primary.light" }}
     >
