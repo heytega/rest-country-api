3a68fd7984ee8d1d5af0e9843b145f01827abfcd
heytega <akpovbiyoranthony@gmail.com>
2023-03-29 00:52:18 +0100

    fixed the background color issue when switching between themes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,60 @@
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import { Box, Paper } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import Countries from "./components/Countries";
-import NavBar from "./components/NavBar";
-import Home from "./routes/Home";
-import Details from "./routes/Details";
-
+import NavBar from './components/NavBar';
+import Home from './routes/Home';
+import Details from './routes/Details';
+import CssBaseline from '@mui/material/CssBaseline';
 // import { ClassNames } from "@emotion/react";
 // import { dark } from "@mui/material/styles/createPalette";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const darkmode = createTheme({
   palette: {
-    mode: "dark",
+    mode: 'dark',
     primary: {
-      main: "hsl(209, 23%, 22%)", //element
-      light: "hsl(207, 26%, 17%)", //background
+      main: 'hsl(209, 23%, 22%)', //element
+      light: 'hsl(207, 26%, 17%)', //background
       // dark: "hsl(200, 15%, 8%)", //text
       // contrastText: "#111517",
     },
 
     text: {
-      primary: "#fff",
+      primary: '#fff',
+    },
+    background: {
+      default: 'hsl(207, 26%, 17%)',
     },
   },
   typography: {
-    fontFamily: "Nunito Sans",
+    fontFamily: 'Nunito Sans',
     fontWeightMedium: 600,
     fontWeightBold: 800,
     // color: "#111517",
     h4: {
-      fontFamily: "Nunito Sans",
+      fontFamily: 'Nunito Sans',
       fontWeight: 800,
     },
     h6: {
-      fontFamily: "Nunito Sans",
+      fontFamily: 'Nunito Sans',
       fontWeight: 700,
-      "@media (max-width:450px)": {
-        fontSize: "0.9rem",
+      '@media (max-width:450px)': {
+        fontSize: '0.9rem',
       },
-      "@media (max-width: 280px)": {
-        fontSize: "0.7rem",
+      '@media (max-width: 280px)': {
+        fontSize: '0.7rem',
       },
     },
     subtitle1: {
       fontWeight: 600,
-      fontSize: "1.1rem",
+      fontSize: '1.1rem',
     },
   },
   subtitle1: {
     fontWeight: 600,
-    fontSize: "1.1rem",
+    fontSize: '1.1rem',
   },
 
   breakpoints: {
@@ -69,23 +72,27 @@ const darkmode = createTheme({
 
 const lightmode = createTheme({
   palette: {
-    mode: "light",
+    mode: 'light',
     common: {
-      black: "hsl(200, 15%, 8%)",
-      white: "#fff",
+      black: 'hsl(200, 15%, 8%)',
+      white: '#fff',
       // DarkBlueT: "#111517",
     },
 
     primary: {
-      main: "#fff", //element
-      light: "hsl(0, 0%, 98%)", //background
+      main: '#fff', //element
+      light: 'hsl(0, 0%, 98%)', //background
       // dark: "hsl(200, 15%, 8%)", //text
       // input: "hsl(0, 0%, 52%)",
       // contrastText: "hsl(0, 0%, 52%)",
     },
 
     text: {
-      primary: "hsl(200, 15%, 8%)",
+      primary: 'hsl(200, 15%, 8%)',
+    },
+
+    background: {
+      default: 'hsl(0, 0%, 98%)',
     },
   },
 
@@ -109,7 +116,7 @@ const lightmode = createTheme({
         root: {
           // Some CSS
           // border: "none",
-          boxShadow: "none",
+          boxShadow: 'none',
         },
       },
     },
@@ -120,55 +127,56 @@ const lightmode = createTheme({
         root: {
           // Some CSS
           // border: "none",
-          backgroundColor: "hsl(0, 0%, 98%)",
+          backgroundColor: 'hsl(0, 0%, 98%)',
         },
       },
     },
   },
 
   typography: {
-    fontFamily: "Nunito Sans",
+    fontFamily: 'Nunito Sans',
     fontWeightMedium: 600,
     fontWeightBold: 800,
     // color: "#111517",
     h4: {
-      fontFamily: "Nunito Sans",
+      fontFamily: 'Nunito Sans',
       fontWeight: 800,
     },
     h6: {
-      fontFamily: "Nunito Sans",
+      fontFamily: 'Nunito Sans',
       fontWeight: 700,
-      "@media (max-width:450px)": {
-        fontSize: "0.9rem",
+      '@media (max-width:450px)': {
+        fontSize: '0.9rem',
       },
-      "@media (max-width: 280px)": {
-        fontSize: "0.8rem",
+      '@media (max-width: 280px)': {
+        fontSize: '0.8rem',
       },
     },
     subtitle1: {
       fontWeight: 600,
-      fontSize: "1.1rem",
+      fontSize: '1.1rem',
     },
   },
   subtitle1: {
     fontWeight: 600,
-    fontSize: "1.1rem",
+    fontSize: '1.1rem',
   },
 });
 
 const App = () => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const [darkMode, setDarkMode] = useState(prefersDarkMode);
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
   return (
     <ThemeProvider theme={darkMode ? darkmode : lightmode}>
+      <CssBaseline />
       <BrowserRouter>
         <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/Details/:countryName" element={<Details />} />
+          <Route exact path='/' element={<Home />} />
+          <Route path='/Details/:countryName' element={<Details />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
