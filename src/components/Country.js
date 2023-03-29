4238ee9d4ea63b5d244ee0d92f11d7fa46225c98
heytega <akpovbiyoranthony@gmail.com>
2023-03-29 01:51:49 +0100

    fixed irregularities in individual flag image ratio shown in each card

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React from 'react';
 // import { motion } from "framer-motion";
-import { Typography, Stack, Card } from "@mui/material";
-import "./Country.css";
+import { Typography, Stack, Card, Box } from '@mui/material';
+// import './Country.css';
 
 const Country = ({ name, population, region, capital, flag }) => {
   return (
-    <Card sx={{ height: "350px", width: 300, backgroundColor: "primary.main" }}>
+    <Card sx={{ height: '350px', width: 300, backgroundColor: 'primary.main' }}>
       {/* width was previously 270 */}
-      {/* <Box
+      <Box
+        component='img'
         sx={{
-          minHeight: "3rem",
-          maxHeight: "13rem",
-          width: 300,
+          minHeight: '3rem',
+          maxHeight: '12rem',
+          minWidth: 300,
+          objectFit: 'cover',
         }}
-      > */}
-      <img src={flag} alt="country's flag" className="image" />
-      {/* </Box> */}
+        alt={`${name}'s flag`}
+        src={flag}
+      />
+      {/* <img src={flag} alt="country's flag" className='image' /> */}
       <Stack sx={{ mt: 2, pl: 2 }}>
         <Typography
-          variant="h6"
-          sx={{ lineHeight: 1, mb: 2, color: "common.DarkBlueT" }}
+          variant='h6'
+          sx={{ lineHeight: 1, mb: 2, color: 'common.DarkBlueT' }}
         >
           {name}
         </Typography>
-        <Typography sx={{ color: "DarkBlueT" }}>
+        <Typography sx={{ color: 'DarkBlueT' }}>
           Population: <span>{population}</span>
         </Typography>
         <Typography>
